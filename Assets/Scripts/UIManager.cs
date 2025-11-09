8d46367963682ed1b0a621a0f910385ecf4acef4
IlyasBen <benyamnailyas@gmail.com>
2025-11-09 05:42:37 +0100

    feat: make canvas follow user and add teleportation area

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -20,14 +20,23 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
+        Debug.Log($"[UIManager] Initializing {managedCanvases.Count} managed canvases");
+     
         // Initialize canvas states according to startActive
         foreach (var uiCanvas in managedCanvases)
         {
             if (uiCanvas.canvas != null)
             {
                 uiCanvas.canvas.gameObject.SetActive(uiCanvas.startActive);
+                Debug.Log($"[UIManager] Canvas '{uiCanvas.name}' initialized - Active: {uiCanvas.startActive}");
+            }
+            else
+            {
+                Debug.LogWarning($"[UIManager] Canvas '{uiCanvas.name}' has null canvas reference!");
             }
         }
+   
+        Debug.Log("[UIManager] Initialization complete");
     }
 
     /// <summary>
@@ -36,11 +45,18 @@ public class UIManager : MonoBehaviour
     /// <param name="canvasName">Name of the canvas to toggle</param>
     public void ToggleCanvas(string canvasName)     
     {
+        Debug.Log($"[UIManager] ToggleCanvas called for: '{canvasName}'");
+        
         UICanvas targetCanvas = managedCanvases.Find(c => c.name == canvasName);
         if (targetCanvas?.canvas != null)
         {
             bool isActive = targetCanvas.canvas.gameObject.activeSelf;
             targetCanvas.canvas.gameObject.SetActive(!isActive);
+            Debug.Log($"[UIManager] Canvas '{canvasName}' toggled from {isActive} to {!isActive}");
+        }
+        else
+        {
+            Debug.LogWarning($"[UIManager] Canvas '{canvasName}' not found or has null canvas reference!");
         }
     }
 
@@ -50,10 +66,17 @@ public class UIManager : MonoBehaviour
     /// <param name="canvasName">Name of the canvas to show</param>
     public void ShowCanvas(string canvasName)
     {
+        Debug.Log($"[UIManager] ShowCanvas called for: '{canvasName}'");
+        
         UICanvas targetCanvas = managedCanvases.Find(c => c.name == canvasName);
         if (targetCanvas?.canvas != null)
         {
             targetCanvas.canvas.gameObject.SetActive(true);
+            Debug.Log($"[UIManager] Canvas '{canvasName}' shown");
+        }
+        else
+        {
+            Debug.LogWarning($"[UIManager] Canvas '{canvasName}' not found or has null canvas reference!");
         }
     }
 
@@ -63,10 +86,17 @@ public class UIManager : MonoBehaviour
     /// <param name="canvasName">Name of the canvas to hide</param>
     public void HideCanvas(string canvasName)
     {
+        Debug.Log($"[UIManager] HideCanvas called for: '{canvasName}'");
+
         UICanvas targetCanvas = managedCanvases.Find(c => c.name == canvasName);
         if (targetCanvas?.canvas != null)
         {
             targetCanvas.canvas.gameObject.SetActive(false);
+            Debug.Log($"[UIManager] Canvas '{canvasName}' hidden");
+        }
+        else
+        {
+            Debug.LogWarning($"[UIManager] Canvas '{canvasName}' not found or has null canvas reference!");
         }
     }
 
@@ -75,13 +105,22 @@ public class UIManager : MonoBehaviour
     /// </summary>
     public void HideAllCanvases()
     {
+        Debug.Log("[UIManager] HideAllCanvases called");
+    
         foreach (var uiCanvas in managedCanvases)
         {
             if (uiCanvas.canvas != null)
             {
                 uiCanvas.canvas.gameObject.SetActive(false);
+                Debug.Log($"[UIManager] Canvas '{uiCanvas.name}' hidden");
+            }
+            else
+            {
+                Debug.LogWarning($"[UIManager] Canvas '{uiCanvas.name}' has null canvas reference!");
             }
         }
+      
+        Debug.Log("[UIManager] HideAllCanvases complete");
     }
 
     /// <summary>
